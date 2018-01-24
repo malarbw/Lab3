e100d4384bee98f8ba51871337a953ee3b38c67f
malarbw <malarbw@gmail.com>
2018-01-24 15:53:49 -0500

    first commit

---
+++ src/Squares_Cubes.java
@@ -0,0 +1,42 @@
+/*
+ * Brad Malarkey
+ * 
+ * Lab 3 -- prompt user to enter integer, display table of squares and cubes
+ */
+import java.util.Scanner;
+
+public class Squares_Cubes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Squares and Cubes table!"); //welcome prompt
+		Scanner input = new Scanner (System.in);
+		System.out.println();
+		String cont = "y"; 
+		
+		while (cont.equalsIgnoreCase("y")) { //use while loop so that program only runs while the user continues to enter y || Y
+			
+			System.out.println("Please enter an integer: "); //prompt for necessary input
+			int num = input.nextInt(); //declare necessary variable
+		
+			System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");       
+            System.out.println("======" + "  " + "======" + "  " + "======"); //mainly for organization in the output
+			
+			for (int i = 1; i <= num; i++) { //use the for loop so that program will only run up to the number the user input
+				int numSquared = (int) Math.pow(i, 2); //Math. method for finding squares and cubes
+				int numCubed = (int) Math.pow(i, 2);
+				
+				String message = "\n" + i + "         " + numSquared + "         " + numCubed;
+				
+				System.out.println(message);
+				System.out.println();
+			}
+			System.out.println("Continue? (y/n): ");
+			cont = input.next();
+		}
+		
+		input.close();
+		System.out.println("Thanks for using the Squares and Cubes table. Goodbye.");//end program
+	}		
+
+}
